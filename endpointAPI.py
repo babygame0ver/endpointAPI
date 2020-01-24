4d08ba2351ffcf5550f7f40e87e9e07974fd41e0
Akshay Kathpal <Akshatkathpal29@gmail.com>
2020-01-24 23:17:38 +0530

    made some changes to endpointAPI

--- endpointAPI.py
+++ endpointAPI.py
@@ -1,62 +1,51 @@
 #!/usr/bin/env python3
-
 from flask import Flask ,jsonify ,abort , make_response ,request
-
 app = Flask(__name__)
 
 
-payloads = [
-    {   'id' : 1,
-        'title' : u'hacker'
-    },
-    {   'id' : 2,
-        'title' : u'developer'
-    }
-]
+class endpointAPI(object):
+    """docstring for ."""
 
-@app.route('/restapi',methods=['GET'])
-def rest_api():
-    return jsonify(payloads)
+    def __init__(self, payloads):
+        self.payloads = payloads
 
+    def Index(self):
+        return 'Hello world'
 
-''' Third Example '''
-payloads = [
-        {
-            'id' : '1'
-        }
-]
+    def json(self):
+        return jsonify(self.payloads)
 
-@app.route('/restapi/<int:api_id>',methods=['GET'])
-def rest_api_with_id(api_id):
-    print(type(api_id))
-    if((api_id)%2 == 0):
-        abort(404,str(api_id))
-    payloads[0]['id'] = api_id # changing id from web
-    return jsonify(payloads)
+    def GitHubCallBack(self):
+        return 'GitHubCallBack'
 
-@app.errorhandler(404)
-def not_found(error): # custom message
-    item = error.description
-    return make_response(jsonify({'error':'{} not found baby'.format(item)}),404)
+    def TwitterCallBack(self):
+        return 'TwitterCallBack'
 
+    def showParamtersFromURl(self,id):
+        return '{} is the paramter in URL'.format(id)
 
-payloads = [{'id':6}]
+payloads = [{'id' : 1, 'title' : u'hacker'},{'id' : 2,'title' : u'developer'}]
+api = endpointAPI(payloads)
 
-@app.route('/restapi/create',methods=['GET','POST'])
-def create_task_with_id():
-    print(request.json,request.headers)
-    if request.method == "GET":
-        payloads[0]['id'] = "GET"
-        return jsonify(payloads,200) # success
-    else: # post method
-        if not request.json or not 'task_id' in request.json:
-            abort(400) # go to not found function
-        payloads[0]['id'] = request.json['task_id'] * 2 # getting data from headers
-        return jsonify(payloads,201) #201 ->created
+@app.route('/',methods=['GET'])
+def index():
+    return api.Index()
 
-@app.errorhandler(400) # can be used when there is POST request available but client is asking for GET
-def not_found_(error):
-    return make_response(jsonify({'error':'bad request baby'}),400)
+@app.route('/json',methods=['GET'])
+def json():
+    return api.json()
+
+@app.route('/gitHubcallback',methods=['GET'])
+def GitHubCallBack():
+    return api.GitHubCallBack()
+
+@app.route('/twittercallBack',methods=['GET'])
+def TwitterCallBack():
+    return api.TwitterCallBack()
+
+@app.route('/urlparam/<int:api_id>',methods=['GET'])
+def showParamtersFromURl(api_id):
+    return api.showParamtersFromURl(api_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
